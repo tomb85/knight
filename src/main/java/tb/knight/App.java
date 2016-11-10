da2ea35114f6e55092b2709dc7161d274fa43516
Tom Brzozowski <tom.b1985@yahoo.co.uk>
2016-11-10 14:57:19 +0000

    Added executable app and deployment

--- src/main/java/tb/knight/App.java
+++ src/main/java/tb/knight/App.java
@@ -1,10 +1,29 @@
 package tb.knight;
 
-/**
- * Hello world!
- */
+import tb.knight.model.MovementTable;
+import tb.knight.model.MovementTableFactory;
+import tb.knight.sequence.SequenceAggregator;
+import tb.knight.sequence.SequenceExtractor;
+
+import java.util.concurrent.ExecutionException;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    private static MovementTable movementTable = MovementTableFactory.movementTable();
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        if (args.length != 1) {
+            System.err.println("Expecting exactly one argument!");
+            System.exit(-1);
+        }
+        int length = Integer.parseInt(args[0]);
+        SequenceAggregator aggregator = new SequenceAggregator();
+        movementTable.getKeys().forEach(k -> aggregator.add(extractorForKey(k, length, 2)));
+        long total = aggregator.getTotal();
+        System.out.println(total);
+    }
+
+    private static SequenceExtractor extractorForKey(Character code, int length, int vowels) {
+        return SequenceExtractor.forMovementTable(movementTable).from(code).length(length).vowels(vowels).create();
     }
 }
